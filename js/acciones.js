e474fe85a3d880bee1cc10bf8c10ae3e9f7bbc7e
AL33F13 <joss.marvell@gmail.com>
2015-04-28 13:23:16 -0500

    dos

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,16 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
+ $('#brnvibrar').on('tap', function() {
+	 navigator.vibrate(1000);
+ }); //tap a btnvibrar
+  $('#btnbeep').on('tap', function(){
+	  navigator.beep(1);
+	  }); //btnbeep
+	  $('#Izquierda').on('swipeleft',
+	  function(){
+		  alert("Barrio a la izquierda");
+	  });
 }); 
 });
 
