55592f3209568de9d72ec59dee3ab3035e23b663
AL33F13 <joss.marvell@gmail.com>
2015-05-06 14:33:30 -0500

    seis
    nuevo

--- js/acciones.js
+++ js/acciones.js
@@ -7,25 +7,30 @@ document.addEventListener("deviceready",function(){
   $('#btnbeep').on('tap', function(){
 	  navigator.notification.beep(1); 
 	  }); //btnbeep
-	  $('#Izquierda').on('swipeleft',
-	  function(){
+	  $('#Izquierda').on('swipeleft',  function(){
 		  alert("Barrio a la Izquierda");
-		  $('#IDerecha').on('swiperifht',
-	  function(){
+		  });
+		  
+		  $('#IDerecha').on('swiperifht',  function(){
 		  alert("Barrio a la Derecha");
+		  });
 		  document.addEventListener("pause", function (){
 			  $('#listado').append("<p> se pauso </p>");
+			  
+			   });
 			  document.addEventListener("resume", function (){
 				  $("#listado").append("<p> se reinicion </p>");
+				   });
+				  
 				  $(window).on('orientationchange', function(e){
 					  $('#listado').oppend("<p> Orietacion: " + e.orientation + "</p>");
 				  });
-			  });
+			 
 				  
-		  });
+		 
 		  
-	  });
-	  });
+	  
+	  
 }); 
 });
 
